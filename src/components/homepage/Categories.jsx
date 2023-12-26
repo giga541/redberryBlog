298ec0c195d28a73bc4b52a52a612601165f0aab
George <gigamikasha@gmail.com>
2023-12-26 17:04:28 +0400

    add select multiple categories logic and button style to show it's active

--- src/components/homepage/Categories.jsx
+++ src/components/homepage/Categories.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import classes from "./categories.module.css";
 
 const Categories = ({ categories, onClick = () => {} }) => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const handleButtonClick = category => {
+    const isSelected = selectedCategories.includes(category.id);
+    const newSelectedCategories = isSelected
+      ? selectedCategories.filter(id => id !== category.id)
+      : [...selectedCategories, category.id];
+
+    setSelectedCategories(newSelectedCategories);
+    onClick(category);
+  };
+
   return (
     <div className={classes.categories}>
       {categories.map(category => {
+        const isSelected = selectedCategories.includes(category.id);
+
         const BUTTON_STYLES = {
           backgroundColor: category.background_color,
           color: category.text_color,
+          border: isSelected ? "1px solid #000" : "none",
         };
 
         return (
@@ -14,7 +30,7 @@ const Categories = ({ categories, onClick = () => {} }) => {
             <button
               style={BUTTON_STYLES}
               className={classes.button}
-              onClick={() => onClick(category)}
+              onClick={() => handleButtonClick(category)}
             >
               {category.title}
             </button>
