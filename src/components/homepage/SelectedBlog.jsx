61af269845ae1dec39319c59dcb2e87b46a5db3a
George <gigamikasha@gmail.com>
2023-12-29 14:52:34 +0400

    start working on SelectedBlog component

--- src/components/homepage/SelectedBlog.jsx
+++ src/components/homepage/SelectedBlog.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { API_TOKEN, API_URL } from "../../consts";
+import Navbar from "../navbar/Navbar";
+import Categories from "./Categories";
+import classes from "./selectedBlog.module.css";
 
 const SelectedBlog = () => {
   const { id } = useParams();
@@ -20,7 +23,18 @@ const SelectedBlog = () => {
         setBlog(data);
       });
   }, []);
-  return <div>SelectedBlog</div>;
+  return (
+    <div>
+      <Navbar />
+      {/* <img src={blog.image} key={id}></img> */}
+      <p className={classes.author}> {blog.author}</p>
+      <p className={classes["publish_date"]}>{blog.publish_date}</p>
+      <h1 className={classes.title}>{blog.title}</h1>
+      {/* <Categories categories={blog.categories} /> */}
+      <h2 className={classes["blog_categories"]}></h2>
+      <p className={classes.description}>{blog.description} description</p>
+    </div>
+  );
 };
 
 export default SelectedBlog;
